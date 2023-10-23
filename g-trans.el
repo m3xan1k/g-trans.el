675a884f37f964c3045bca3fb29bd777ccff9eb0
m3xan1k <m3xan1k@duck.com>
2023-10-23 22:32:10 +0300

    version 2.0, get rid of 3rd party deps

--- g-trans.el
+++ g-trans.el
@@ -6,7 +6,7 @@
 ;; Keywords: translation, dictionary
 ;; Version: 0.2
 
-;; Package-Requires: ((emacs "27.1") (request "0.3.3"))
+;; Package-Requires: ((emacs "27.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -32,30 +32,39 @@
 
 (require 'url)
 (require 'json)
-(require 'cl-lib)
 
 (defconst g-trans--base-url "https://translate.googleapis.com/translate_a/single")
+(defvar g-trans-default-source-lang "ru")
+(defvar g-trans-default-target-lang "en")
 
-(defun g-trans--params->query-string (params)
-  (concat "?" (mapconcat
-               (lambda (pair)
-                 (format "%s=%s" (car pair) (url-hexify-string (cdr pair))))
-               params
-               "&")))
+;; Alist to query string
+;; --------------------------------------------
+(defun g-trans--params-to-query-string (params)
+  "Encode alist to query string.
+PARAMS - alist - key-value pairs of GET params."
+  (concat "?"
+	  (mapconcat
+	   (lambda (pair)
+	     (format "%s=%s" (car pair) (url-hexify-string (cdr pair))))
+	   params
+	   "&")))
 
-;; (cl-assert (string=
-;;             (g-trans--params->query-string '((a . "1") (b . "2") (c . "3")))
-;;             "?a=1&b=2&c=3"))
-
-(defun g-trans--send-request (query)
+;; Send http request
+;; ---------------------------------------------------------
+(defun g-trans--make-request (query source-lang target-lang)
+  "Make request to googleapi.
+QUERY - string - word or phrase to translate.
+SOURCE-LANG - string - source language code(iso3166-1-alpha-2).
+TARGET-LANG - string - target language code(iso3166-1-alpha-2).
+There's no official documentation about which languages are available."
   (let* ((url-request-method "GET")
-         (params `((sl . "en")
-                   (tl . "ru")
+         (params `((sl . ,source-lang)
+                   (tl . ,target-lang)
 		   (client . "gtx")
 		   (dt . "t")
 		   (q . ,query)))
          (url (concat g-trans--base-url
-                      (g-trans--params->query-string params))))
+                      (g-trans--params-to-query-string params))))
     (with-current-buffer (url-retrieve-synchronously url)
       (set-buffer-multibyte t)
       (goto-char url-http-end-of-headers)
@@ -63,11 +72,46 @@
 	(kill-buffer)
 	data))))
 
+;; Parse json response
+;; ----------------------------------------
 (defun g-trans--get-translate-result (data)
+  "DATA - json response from googleapi.
+There is no documentation.
+But successful json-response from googleapi looks smth like
+[[[translated_result, ...]]]
+so just recursively get this result"
   (when (not (list data)) "No results")
   (if (stringp data)
-      data
+      (print data)
       (g-trans--get-translate-result (aref data 0))))
+
+;; Main function
+;; ---------------------------------------------------------------
+(defun g-trans-translate (query &optional source-lang target-lang)
+  "Translates given query from source-lang to target-lang.
+QUERY - string - word or phrase to translate.
+SOURCE-LANG - string - source language code.
+TARGET-LANG - string - target language code.
+
+There's no official documentation about which
+languages are available.
+
+If source-lang/target-lang not provided,
+then global defaults are used."
+  (interactive (list
+		(read-from-minibuffer "Query: ")
+		(read-from-minibuffer
+		 (format "Source lang[%s]: " g-trans-default-source-lang))
+		(read-from-minibuffer
+		 (format "Target lang[%s]: " g-trans-default-target-lang))))
+  (let ((source-lang (if (string= source-lang "")
+			 g-trans-default-source-lang
+		       source-lang))
+	(target-lang (if (string= target-lang "")
+			 g-trans-default-target-lang
+		       target-lang)))
+    (g-trans--get-translate-result
+     (g-trans--make-request query source-lang target-lang))))
 
 (provide 'g-trans)
 ;;; g-trans.el ends here
